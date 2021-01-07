67f9183b1a972d7e3fc92691139e30ce32485521
Tyler Silverman <silverman.tyler@gmail.com>
2021-01-07 14:42:45 -0500

    calloborated with caitlin

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    console.log("login ")
+    console.log("You are now Logged in", user.name)
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
