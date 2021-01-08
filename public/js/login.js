69a24d5aaf05e4de6db0e7924daaec45d3c99506
Caitlin Crews <caitlinrcrews@gmail.com>
2021-01-07 20:25:05 -0500

    updated login

--- public/js/login.js
+++ public/js/login.js
@@ -16,7 +16,7 @@ const loginFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/dashboard');
     } else {
-      document.location.replace('/login');
+      // document.location.replace('/login');
       alert('Failed to log in.');
     }
   }
