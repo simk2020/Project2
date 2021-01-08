5dd21e9d35a0637a8083b5cf76b8f281f923fb4a
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-08 00:51:21 -0500

    tentative logic

---
+++ public/js/activityLogic.js
@@ -0,0 +1,14 @@
+const activity1=async UX=>{
+    UX.preventDefault();
+
+    const iQuestion=document.querySelector('#uiLogic').value;
+    const iChoice=document.querySelector('#uxLogic').value;
+
+    if(iQuestion&&iChoice{
+        const userRequest=await fetch(``)
+    })
+}
+
+//handlebars logic
+    //if question is boolean, campare for true or false
+    //if question is multichoice, compare for corrrect response property
