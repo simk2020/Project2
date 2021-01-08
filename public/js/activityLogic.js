bdb25c34d2ea3af79395431b8eb3b90534950267
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-08 03:39:21 -0500

    version control

--- public/js/activityLogic.js
+++ public/js/activityLogic.js
@@ -4,11 +4,21 @@ const activity1=async UX=>{
     const iQuestion=document.querySelector('#uiLogic').value;
     const iChoice=document.querySelector('#uxLogic').value;
 
-    if(iQuestion&&iChoice{
-        const userRequest=await fetch(``)
-    })
+    if(iQuestion&&iChoice){
+        let userChoice;
+        userChoice=iChoice.userChoice.value;
+        if(!this.userChoice){
+            alert('Practice always lends itself to progress');
+        }else{
+            alert("I would call you a genius but I don't want to inflate your false sense of self");
+        }
+    }
 }
+
 
 //handlebars logic
     //if question is boolean, campare for true or false
     //if question is multichoice, compare for corrrect response property
+    //must account for variable question types
+//upon click of submit button, configure what that action results in
+//add event listeners on targeted elements
